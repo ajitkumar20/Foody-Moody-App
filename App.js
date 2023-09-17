4d78bef47dee9616409df8776b556f110d0e6bce
Ajit <1941012954.i.ajitkumar@gmail.com>
2023-09-17 12:49:25 +0530

    Third class practical

--- App.js
+++ App.js
@@ -1,29 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-    "h1",
-    {
-      id: "title",
-    },
-    "Heading 1 for Parcel"
+// React.createElement => Object => HTML(DOM)
+
+// Functional Component
+const Title = () => (
+  <h1 id="title" key="h2">
+    Namaste React
+  </h1>
+);
+
+// JSX => React.createElement => Object => HTML(DOM)
+const heading1 = (
+  <h1 id="title" key="h1">
+    Namaste React
+  </h1>
+);
+
+// React Components:- 1. Functional Component (latest), 2. Class Based Component (old)
+
+//Functional Component
+const HeaderComponent = () => {
+  return (
+    <div>
+      <Title />
+      <h2>Namaste React functional component</h2>
+      <h2>This is a h2 tag</h2>
+    </div>
   );
-  const heading2 = React.createElement(
-    "h2",
-    {
-      id: "title",
-    },
-    "Heading 2"
-  );
-  const container = React.createElement(
-    "div",
-    {
-      id: "container",
-    },
-    [heading, heading2]
-  );
-  console.log(heading);
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  //passing a react element inside the root
-  //async defer
-  root.render(container);
+};
+
+// console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+//passing a react element inside the root
+//async defer
+root.render(<HeaderComponent />);
